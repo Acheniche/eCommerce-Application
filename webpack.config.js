076d0e9e9bb2bf0fbf0fb418a7fa5fa83998243d
Acheniche <ache.niche00@mail.ru>
2023-08-04 00:01:34 +0300

    feat: add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ const path = require('path');
 module.exports = {
   entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
 };
