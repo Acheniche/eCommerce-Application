89b91cb268537fb4ee33cc94828942b8f44d40e0
Acheniche <ache.niche00@mail.ru>
2023-08-24 01:16:32 +0300

    feat: add default setting of billing and shipping addresses

--- src/components/registrationPage/reg.ts
+++ src/components/registrationPage/reg.ts
@@ -3,24 +3,34 @@ import PopupWindow from '../../utils/templates/popup';
 import App, { PagesID } from '../app';
 import Header from '../header/header';
 
-async function addOnServ(check: string) {
+async function addOnServ(check_billing: string, check_shipping: string) {
   const email = document.getElementById('registration-email') as HTMLInputElement;
   const password = document.getElementById('registration-password') as HTMLInputElement;
   const firstname = document.getElementById('registration-firstname') as HTMLInputElement;
   const lastname = document.getElementById('registration-lastname') as HTMLInputElement;
   const dateOfBirth = document.getElementById('registration-dateOfBirth') as HTMLInputElement;
-  const street = document.getElementById('registration-street') as HTMLInputElement;
-  const city = document.getElementById('registration-city') as HTMLInputElement;
-  const postal = document.getElementById('registration-postal') as HTMLInputElement;
-  const country = document.getElementById('country') as HTMLInputElement;
+  const billing_street = document.getElementById('registration-street_billing') as HTMLInputElement;
+  const billing_city = document.getElementById('registration-city_billing') as HTMLInputElement;
+  const billing_postal = document.getElementById('registration-postal_billing') as HTMLInputElement;
+  const billing_country = document.getElementById('country_billing') as HTMLInputElement;
+  const shipping_street = document.getElementById('registration-street_shipping') as HTMLInputElement;
+  const shipping_city = document.getElementById('registration-city_shipping') as HTMLInputElement;
+  const shipping_postal = document.getElementById('registration-postal_shipping') as HTMLInputElement;
+  const shipping_country = document.getElementById('country_shipping') as HTMLInputElement;
   const popupWindow = new PopupWindow();
   const logoutBtn = new Header('header', 'header');
   const logoutBtnListener = new LogoutButton();
-  let countryData = '';
-  if (country.value === 'USA') {
-    countryData = 'US';
+  let billing_countryData = '';
+  let shipping_countryData = '';
+  if (billing_country.value === 'USA') {
+    billing_countryData = 'US';
   } else {
-    countryData = 'DE';
+    billing_countryData = 'DE';
+  }
+  if (shipping_country.value === 'USA') {
+    shipping_countryData = 'US';
+  } else {
+    shipping_countryData = 'DE';
   }
   const data = {
     email: email.value.toString(),
@@ -28,26 +38,24 @@ async function addOnServ(check: string) {
     lastName: lastname.value.toString(),
     password: password.value.toString(),
     dateOfBirth: dateOfBirth.value.toString(),
-    action: check == 'true' ? 'setDefaultBillingAddress' : 'addAddress',
-    AddressID:
-      check == 'true'
-        ? [
-            {
-              country: countryData,
-              city: city.value.toString(),
-              street: street.value.toString(),
-              postalCode: postal.value.toString(),
-            },
-          ]
-        : '',
-    addresses: [
+      addresses: [
       {
-        country: countryData,
-        city: city.value.toString(),
-        street: street.value.toString(),
-        postalCode: postal.value.toString(),
+        country: billing_countryData,
+        city: billing_city.value.toString(),
+        streetName: billing_street.value.toString(),
+        postalCode: billing_postal.value.toString(),
+      },
+      {
+        country: shipping_countryData,
+        city: shipping_city.value.toString(),
+        streetName: shipping_street.value.toString(),
+        postalCode: shipping_postal.value.toString(),
       },
     ],
+    ...(check_billing === "true" && {defaultBillingAddress: 0}),
+    ...(check_shipping === "true" && {defaultShippingAddress: 1}),
+        billingAddresses: [0],
+        shippingAddresses: [1],
   };
   const response = await fetch(
     'https://auth.europe-west1.gcp.commercetools.com/oauth/token?grant_type=client_credentials',
@@ -95,13 +103,24 @@ async function addOnServ(check: string) {
     },
   );
 }
-function setDefaultAdress() {
-  if ((<HTMLInputElement>document.querySelector('.change-check')).checked == false) {
+
+function setDefaultAdress_billing() {
+  if ((<HTMLInputElement>document.querySelector('.change-check_billing')).checked == false) {
     return false;
   } else {
     return true;
   }
 }
+
+function setDefaultAdress_shipping() {
+  if ((<HTMLInputElement>document.querySelector('.change-check_shipping')).checked == false) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
 export default function registrationOnServ() {
   (<HTMLInputElement>document.querySelector('.registration-button')).addEventListener('click', (e) => {
     e.stopImmediatePropagation();
@@ -113,11 +132,11 @@ export default function registrationOnServ() {
         counter += 1;
       }
       if (counter == 9) {
-        const check = setDefaultAdress();
-        addOnServ(String(check));
+        const check_billing = setDefaultAdress_billing();
+        const check_shipping = setDefaultAdress_shipping();
+        addOnServ(String(check_billing), String(check_shipping));
       }
       i += 1;
     }
   });
 }
-
