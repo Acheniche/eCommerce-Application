5fa2cfb75af007b0d2573db3d447c2d106e899f0
Acheniche <ache.niche00@mail.ru>
2023-09-02 02:18:47 +0300

    feat: add navigation

--- src/components/catalogProductPage/catalogProductPage.ts
+++ src/components/catalogProductPage/catalogProductPage.ts
@@ -1,7 +1,7 @@
 import Page from '../../utils/templates/page';
 import './style.css';
 import CreateCatalogPage from '../../utils/templates/catalogPageTemplate';
-import { getProducts } from './products';
+import { createProductsCards, createProductsCardsCategory, getProducts, getSubCategoryProduct } from './products';
 
 
 class CatalogPage extends Page {
@@ -18,40 +18,30 @@ class CatalogPage extends Page {
     //this.container.append(title);
     this.container.insertAdjacentHTML('beforeend', catalog.mainCatalog());
     getProducts().then((data) => {
-      const products = document.querySelector('.products');
-      for (let i = 0; i < data.results.length; i++) {
-        const cardWrapper = document.createElement('div');
-        cardWrapper.classList.add('cardWrapper');
-        const img = document.createElement('img');
-        img.src = `${data.results[i].masterData.staged.masterVariant.images[0].url}`;
-        const name = document.createElement('h3');
-        name.classList.add('productName');
-        name.textContent = `${data.results[i].masterData.current.name['en-US']}`;
-        const description = document.createElement('p');
-        description.classList.add('productDescription');
-        description.textContent = `${data.results[i].masterData.current.description['en-US']}`;
-        const price = document.createElement('h3');
-        price.classList.add('productPrice');
-        const priceValue: string = data.results[i].masterData.staged.masterVariant.prices[0].value.centAmount;
-        price.textContent = `${(priceValue)} ${data.results[i].masterData.staged.masterVariant.prices[0].value.currencyCode}`;
-
-        cardWrapper.append(img);
-        cardWrapper.append(name);
-        cardWrapper.append(description);
-        cardWrapper.append(price);
-
-        if (data.results[i].masterData.staged.masterVariant.prices[0].discounted) {
-          price.style.textDecoration = 'line-through';
-          const discount = document.createElement('h3');
-          discount.classList.add('discount');
-          discount.textContent = `${data.results[i].masterData.staged.masterVariant.prices[0].discounted.value.centAmount} ${data.results[i].masterData.staged.masterVariant.prices[0].discounted.value.currencyCode}`;
-          cardWrapper.append(discount);
-        }
+      createProductsCards(data);
+    });
+    document.body.addEventListener('click', e => {
+      if (e.target as HTMLElement) {
+      if ((e.target as HTMLElement).tagName === 'A') {
+        document.querySelectorAll('A').forEach(i => {
+          i.classList.remove('active');
+        });
+        (e.target as HTMLElement).classList.add('active');
+        console.log((e.target as HTMLElement).id);
+        const products = document.querySelector('.products');
         if (products) {
-          products.append(cardWrapper);
+        products.innerHTML = '';
+        }
+        getSubCategoryProduct((e.target as HTMLElement).id).then((data) => {
+          createProductsCardsCategory(data);
+        });
+        if ((e.target as HTMLElement).id === 'main') {
+          getProducts().then((data) => {
+            createProductsCards(data);
+          });
         }
       }
-      console.log(data.results[0]);
+    }
     });
     return this.container;
   }
--- src/utils/templates/catalogPageTemplate.ts
+++ src/utils/templates/catalogPageTemplate.ts
@@ -9,7 +9,29 @@ export default class CreateCatalogPage {
         return `
         <div class="filters"></div>
         <div class="rightSide">
-            <div class="navigation"></div>
+            <div class="navigation">
+            <ul class="column1">
+                <li><a class="category" id="main">Main</a></li>
+            </ul>
+            <ul class="column2">
+                <li><a class="category" id="bc68bcc7-8b89-4bd2-b11a-552252a92b21">Cell phones and accessories</a></li>
+                <li><a id="c481c62a-318b-45d3-bef7-feff154c9d58">Smartphones</a></li>
+                <li><a id="7fae1263-6720-4748-a42b-3fa6bae2ef02">Smartwatches</a></li>
+                <li><a id="f055b7b1-9aab-4022-b988-705412319ff8">Cases, Covers and Holsters</a></li>
+            </ul>
+            <ul class="column3">
+                <li><a class="category" id="57fd3f41-95f6-4191-b6e5-afbb49cc2b86">Computers</a></li>
+                <li><a id="a10a5c6a-f11b-4010-848b-b3607430dafc">Laptops</a></li>
+                <li><a id="12506152-c53b-4235-972f-c3029b4be43b">PCs</a></li>
+                <li><a id="04150097-8413-4d6f-b06c-bc9dcc2d3a56">Printers and Scanners</a></li>
+            </ul>
+            <ul class="column4">
+                <li><a class="category" id="a9a76eea-80ff-44cf-8e37-eb125e85c096">TVs and accessories</a></li>
+                <li><a id="45efac34-f6fd-46a8-ba10-97ef1ec270a2">Televisions</a></li>
+                <li><a id="27178ae5-aba8-4ffc-aac2-d80d60829b08">Projectors</a></li>
+                <li><a id="968de089-75e0-4d7c-8428-4b2bd0890547">Home Cinema</a></li>
+            </ul>
+            </div>
             <div class="products"></div>
         </div>
         `;
